cd8bc1eaaa41f52fcd15967816327973f05f80cb
Talha Zafar <49996963+talhazafarjutt@users.noreply.github.com>
2023-06-14 13:34:32 +0500

    docker configration

--- server/database.py
+++ server/database.py
@@ -5,8 +5,8 @@ from .models.user import User
 
 async def init_db():
     client = motor.motor_asyncio.AsyncIOMotorClient(
-        "mongodb://localhost:27017/LD"
+        "mongodb://localhost:27017/"
     )
-
+    #replace localhost with database container name to connect it.
     await init_beanie(database=client.LD, document_models=[User])
     print('database connected with mongoDB')
