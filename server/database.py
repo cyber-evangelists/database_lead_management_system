e96340951a9292199670fe633508780c49015abf
Talha Zafar <49996963+talhazafarjutt@users.noreply.github.com>
2023-06-15 09:52:14 +0500

    add env variable and debug vs code config

--- server/database.py
+++ server/database.py
@@ -1,11 +1,15 @@
 from beanie import init_beanie
 import motor.motor_asyncio
 from .models.user import User
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+mongo_uri = os.getenv('MONGO_HOST_URI')
 
 async def init_db():
     client = motor.motor_asyncio.AsyncIOMotorClient(
-        "mongodb://localhost:27017/"
+        f"mongodb://{mongo_uri}:27017"
     )
     #replace localhost with database container name to connect it.
     await init_beanie(database=client.LD, document_models=[User])
